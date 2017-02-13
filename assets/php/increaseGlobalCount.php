d77644fab7f8e64c622f75b9ab8bc45c7de0ca3b
Bogdan Ionita <bogdan.ionita96@gmail.com>
2017-02-13 14:34:54 +0100

    increaseGlobalCount fix

--- assets/php/increaseGlobalCount.php
+++ assets/php/increaseGlobalCount.php
@@ -4,7 +4,7 @@
 	$conn = new mysqli($servername, $usn, $psw,$database);
 	if($conn->connect_errno)$ans =  'There was a problem with connecting to our server. Please, try again!';
 	else{
-			$query="UPDATE counters SET value=value+1 WHERE name='globalCounter'";
+			$query="UPDATE counters SET value=value+$_POST['count'] WHERE name='globalCounter'";
 			$conn->query($query);
 	}
 	
