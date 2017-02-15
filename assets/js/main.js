d02a08f0fea17f9fdac16964141e2431771794cc
Bogdan Ionita <bogdan.ionita96@gmail.com>
2017-02-15 18:57:54 +0000

    Refactor the game using Phaser IO

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,163 +1,144 @@
-//Create the renderer
-var renderer = PIXI.autoDetectRenderer(  256, 256,
-  {antialias: false, transparent: true, resolution: 1}
-);
-renderer.view.style.position = "absolute";
-renderer.view.style.display = "block";
-renderer.autoResize = true;
-renderer.resize(parseInt(window.innerWidth*0.98), parseInt(window.innerHeight*0.98));
+var game = new Phaser.Game('100', '100', Phaser.AUTO, 'gameContainer', 
+						   { preload: preload, create: create, update: update, render: render}, 
+						   true);
 
-document.body.appendChild(renderer.view);
-var stage = new PIXI.Container();
-var globalCounterContainer = new PIXI.Container();
-var nameContainer = new PIXI.Container();
-var trashText = new PIXI.Text("TRASH",
-  {fontFamily: "Arial Black", fontSize: 70, fill: "cyan"});
-var doveText = new PIXI.Text("DOVE",
-  {fontFamily: "Arial Black", fontSize: 70, fill: "red"});
-var orgText = new PIXI.Text(".ORG",
-  {fontFamily: "Arial Black", fontSize: 70, fill: "white"});
-var tink = new Tink(PIXI, renderer.view);
-var pointer = tink.makePointer();
-var trashdove =  new Array();
-var state = play;
-var remainingAnimationTime = 0;
-var tapped = 0;
-var tappedCount = 0;
-var totalCounter;
-var gif = new PIXI.Container();
-var currentFrame = 1;
+
+
+var socialMediaGroup;
+
+var globalCounterGroup;
+var globalCounterText, slamsText;
+
+var titleGroup;
+var trashText,doveText, orgText;
+
+var trashdove;
+var animationRemainingTime = 0;
+
 var song;
-var soundVolume=1, musicVolume=1;
-var globalCounter = new PIXI.Text("",
-  {fontFamily: "Arial Black", fontSize: 60, fill: "white"}
-);
-var textSlams = new PIXI.Text("SLAMS",
-  {fontFamily: "Arial Black", fontSize: 60, fill: "grey"});
-  
+var background;
 
-pointer.tap = () => tapped=1;
-
-PIXI.loader
-  .add([
-    "assets/img/trashdove1.png",
-    "assets/img/trashdove2.png",
-    "assets/img/trashdove3.png",
-    "assets/img/trashdove4.png",
-    "assets/img/trashdove.gif",
-    "assets/img/bg.jpg"
-  ])
-  .load(setup);
-
-sounds.load([
-	"assets/sound/song.mp3"
-]);
+var recentTapped = 0;
+var globalCounter = 0;
 
 
-
-  
-function setup() {
-  trashdove[1] = new PIXI.Sprite(PIXI.loader.resources["assets/img/trashdove1.png"].texture);
-  trashdove[2] = new PIXI.Sprite(PIXI.loader.resources["assets/img/trashdove2.png"].texture);
-  trashdove[3] = new PIXI.Sprite(PIXI.loader.resources["assets/img/trashdove3.png"].texture);
-  trashdove[4] = new PIXI.Sprite(PIXI.loader.resources["assets/img/trashdove4.png"].texture);
-  for (var i in trashdove)
-  {
-	trashdove[i].draggable = false;
-	trashdove[i].visible = false;
-	gif.addChild(trashdove[i]);
-  }
-  sounds.whenLoaded = function (){
-	  song = sounds["assets/sound/song.mp3"];
-	  song.loop = true;
-	  song.play();
-  }
-  nameContainer.addChild(trashText);
-  nameContainer.addChild(doveText);
-  nameContainer.addChild(orgText);
-  globalCounterContainer.addChild(globalCounter);
-  globalCounterContainer.addChild(textSlams);
-  stage.addChild(gif);  
-  stage.addChild(globalCounterContainer);
-  stage.addChild(nameContainer);
-  getCount();
-  setInterval(getCount, 5300);
-  setInterval(pushCount, 13700);
-  setInterval(resetTapped,1000);
-  gameLoop();
-  
+function preload() {
+	game.load.image('background', 'assets/img/bg.jpg');
+    game.load.spritesheet('trashdove', 'assets/img/trashdove6.png', 350, 400);
+	game.load.audio('song',['assets/sound/song.ogg','assets/sound/song.m4a']);
 }
 
-function gameLoop(){
-
-  requestAnimationFrame(gameLoop);
-  tink.update();
-  state();
-
-  gif.x=window.innerWidth/2 - gif.width/2;
-  gif.y=window.innerHeight/2- gif.height/2;
-  doveText.x = parseInt(trashText.width * 1.05);
-  orgText.x = parseInt(doveText.x+ doveText.width * 1.05);
-  
-  textSlams.y= parseInt(globalCounter.height * 1.07);
-  globalCounter.text = (totalCounter+tappedCount).toString();
-  nameContainer.y = parseInt(window.innerHeight*0.1);
-  nameContainer.x = parseInt((window.innerWidth - nameContainer.width)/2);
-  
-  globalCounterContainer.y = parseInt(window.innerHeight*0.78);
-  globalCounterContainer.x = parseInt(window.innerWidth * 0.03);
-  
-  renderer.render(stage);
-}
-
-function play()
+function spriteScale(object, scale)
 {
+	var x = Math.min(game.width/object.width * scale, game.height/object.height * scale);
+	object.scale.setTo(x, x);
+}
+
+
+function create() {
+	game.physics.startSystem(Phaser.Physics.ARCADE);
+	game.stage.disableVisibilityChange = true;
+	game.time.advancedTiming = true;
+	//game.time.desiredfps = 60;
+	
+	background = game.add.sprite(game.world.centerX, game.world.centerY, 'background');
+	background.anchor.setTo(0.5, 0.5);	
+	background.scale.setTo(game.width/background.width, game.height/background.height);
+	
+	trashdove = game.add.sprite(game.world.centerX, game.world.centerY, 'trashdove');
+	trashdove.anchor.setTo(0.5, 0.5);
+	spriteScale(trashdove, 0.3);
+	trashdove.animations.add('slam', [0, 1, 2, 3, 4, 5], 30, true);
+	trashdove.draggable = false;
+	trashdove.inputEnabled = true;
+	trashdove.input.useHandCursor = true;
+	trashdove.events.onInputDown.add(clickTrash);
+	
+	globalCounterGroup = game.add.group();
+	globalCounterText = game.add.text(0, 0, "1000000",{fontFamily: "Arial Black", fontSize: 200, fill: "white"});
+	slamsText = game.add.text(0, 0, "SLAMS",{fontFamily: "Arial Black", fontSize: 200, fill: "grey"});
+	globalCounterGroup.add(globalCounterText);
+	globalCounterGroup.add(slamsText);
+	slamsText.y = globalCounterText.height * 0.7;
+	spriteScale(globalCounterGroup,0.25);
+	globalCounterGroup.y = game.world.height - globalCounterGroup.height;
+	globalCounterGroup.x = game.world.width * 0.01;
+	
+	titleGroup = game.add.group();	
+	trashText = game.add.text(0, 0, "TRASH",{fontFamily: "Arial Black", fontSize: 200, fill: "cyan"});
+	doveText = game.add.text(0, 0, "DOVE",{fontFamily: "Arial Black", fontSize: 200, fill: "red"});
+	orgText = game.add.text(0, 0, ".ORG",{fontFamily: "Arial Black", fontSize: 200, fill: "white"});
+	titleGroup.add(trashText);
+	titleGroup.add(doveText);
+	titleGroup.add(orgText);
+	doveText.x = trashText.width * 1.03;
+	orgText.x = trashText.width + doveText.width * 1.03;
+	spriteScale(titleGroup,0.60);
+	titleGroup.x = game.world.centerX - titleGroup.width * 0.5;
+	titleGroup.y = game.world.height * 0.05;
+	
+	
+	
+	socialMediaGroup = game.add.group();
+	
+	song = game.add.audio('song',0,true);
+	song.play();
+	
+	getCount();
+    getCountTimer = game.time.events.loop(5000, getCount);
+	pushCountTimer = game.time.events.loop(10000, pushCount);
+	resetTappedTimer = game.time.events.loop(1000, resetTapped);
+}
+
+function update() {
+	
+	if (animationRemainingTime==0) 
+	{		
+		trashdove.animations.stop();
+		trashdove.frame = 0;
+	} else {
+		animationRemainingTime--;
+		 }
+	
+	
 	if (song)
 		song.volume = song.volume * 0.95;
-	if (remainingAnimationTime>0)
-	{
-		remainingAnimationTime--;
-		
-	}	
 	
-	trashdove[currentFrame].visible = false;	
-	currentFrame = parseInt(remainingAnimationTime/4)  + 1;
-	trashdove[currentFrame].visible = true;
 	
-	if (pointer.hitTestSprite(gif))
-	{
-		if (tapped)
-		{
-			if (song)
-				if (song.volume < 2) song.volume = 2;
-			tappedCount++;
-			tapped = 0;
-			//jumpSound();
-			remainingAnimationTime = 12;
-			
-		}
-	}
 }
+
+function render() {
+	globalCounterText.text = (globalCounter+recentTapped).toString();
+	//game.debug.text(game.time.fps || '--', 2, 14, "#00ff00");   
+}
+
 function pushCount()
 {
 	$.ajax({
-			data: {count: tappedCount},
+			data: {count: recentTapped},
 			url: 'assets/php/increaseGlobalCount.php',
 			method: 'POST'
 			});
 	
 }
 
+function clickTrash()
+{
+	recentTapped++;
+	animationRemainingTime =  12;
+	trashdove.animations.play('slam');
+	song.volume = 2;
+}
+
 function getCount()
 {
 	pushCount();
 	$.ajax({
-			data: {count: tappedCount},
 			url: 'assets/php/getGlobalCount.php',
-			method: 'POST', 
+			method: 'GET', 
 			success: function(value) {
-				totalCounter = parseInt(value);
-				tappedCount = 0;
+				globalCounter = parseInt(value);
+				recentTapped = 0;
 				}
 			});
 	
@@ -165,23 +146,5 @@ function getCount()
 }
 function resetTapped()
 {
-	tapped = 0;
+	recentTapped = 0;
 }
-function jumpSound() {
-  soundEffect(
-    523.25,       //frequency
-    0.05,         //attack
-    0.2,          //decay
-    "sine",       //waveform
-    soundVolume,            //volume
-    0.8,          //pan
-    0,            //wait before playing
-    600,          //pitch bend amount
-    true,         //reverse
-    100,          //random pitch range
-    0,            //dissonance
-    undefined,    //echo array: [delay, feedback, filter]
-    undefined     //reverb array: [duration, decay, reverse?]
-  );
-}
-
